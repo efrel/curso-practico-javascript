8e3fb1ea542486afb4f6a35667420691d2e1421d
efrel <elfen95.ealc@gmail.com>
2022-02-16 10:12:25 -0600

    clase 12: Escribiendo HTML desde JavaScript

--- descuentos.js
+++ descuentos.js
@@ -4,3 +4,14 @@ function calcularPrecioConDescuento(precio, descuento) {
 
   return precioConDescuento;
 }
+
+function onClickButtonPriceDiscount() {
+  const precio = document.getElementById("InputPrice").value;
+  const descuento = document.getElementById("InputDiscount").value;
+
+  const precioConDescuento = calcularPrecioConDescuento(precio, descuento);
+
+  const resultP = document.getElementById("ResultP");
+
+  resultP.innerText = `El precio con descuento es: $${precioConDescuento}`;
+}
